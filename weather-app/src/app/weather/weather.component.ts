4b771fe18299b6ef32a30df06b04310fec3e07aa
Carolina Campos <carolinacampos.dev@gmail.com>
2024-07-18 16:32:52 -0300

    Criado o componente de clima

--- weather-app/src/app/weather/weather.component.ts
+++ weather-app/src/app/weather/weather.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { WeatherService } from '../weather.service';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
 })
 export class WeatherComponent {
 
+    city: string = '';
+    weatherData: any;
+
+    constructor(private weatherService: WeatherService) { }
+
+    getWeather() {
+      this.weatherService.getWeather(this.city).subscribe(
+        data => this.weatherData = data,
+        error => console.error('Error:', error)
+      );
+    }
 }
