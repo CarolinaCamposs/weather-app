77720362cb22503e8f43d20bf43094930f0f3b8a
Carolina Campos <carolinacampos.dev@gmail.com>
2024-07-18 17:08:29 -0300

    Novos imports do Angular Material

--- weather-app/src/main.ts
+++ weather-app/src/main.ts
@@ -4,11 +4,14 @@ import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent,{
   providers: [
     provideHttpClient(),
-    provideRouter(routes)
+    provideRouter(routes), 
+    provideAnimationsAsync()
   ]
 })
   .catch((err) => console.error(err));
